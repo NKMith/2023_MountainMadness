e1edc890bd7a315f8dbab069ca75da5f90256cb7
Geon Woo Kim <gwk1@sfu.ca>
2023-02-18 13:02:50 -0800

    Added getFunctionDeclaration

--- interpreter.py
+++ interpreter.py
@@ -15,12 +15,20 @@ Translate
 INPUT_FILENAME = "inputText.txt"
 OUTPUT_FILENAME = "output.py"
 
+SYNTAX_DICTIONARY = {
+    "var" : "myhomie",
+    "def" : "fudge",
+    "for" : "fur"
+}
+
+
 class Translator:
     def __init__(self, inputFileName :str, outputFileName :str):
         self.file = open(inputFileName, "r")
-        self.outputFile = open(outputFileName, "a")
+        self.outputFile = open(outputFileName, "w+")
         self.currentLine = ""
         self.currentWordList = ""
+
 
     def translate(self):
         print("okay")
@@ -31,19 +39,59 @@ class Translator:
             print(line)
             self.currentLine = line
             self.currentWordList = self.currentLine.split()
-            self.determineWhichStatement()
 
+            outputStr = self.getTab()
+            outputStr += self.determineWhichStatement()
+            print(outputStr)
+
+            self.outputFile.write(outputStr + '\n')
+
+    def getTab(self):
+        tabStr = ""
+        count = 0
+        while self.currentLine[count] == " ":
+            tabStr += " "
+            count += 1
+        return tabStr
 
     def determineWhichStatement(self):
         # bunch of different if statements
-        firstWord = self.currentWordList[0]
-        if firstWord == "myHomie":
-            self.writeVarDeclaration()
+        firstWord = self.currentWordList[0].lower()
+        if firstWord == SYNTAX_DICTIONARY["var"]:
+            return self.getVarDeclaration()
+        elif firstWord == SYNTAX_DICTIONARY["def"]:
+            return self.getFunctionDeclaration()
+        
+        else:
+            return "ERROR - NO SYNTAX MATCHED"
 
-    def writeVarDeclaration(self):
+    def getVarDeclaration(self):
         # myHomie i = 100
-        outputStr = f"{self.currentWordList[1]} = {self.currentWordList[3]}"
-        self.outputFile.write(outputStr)
+        print("VAR")
+        return f"{self.currentWordList[1]} = {self.currentWordList[3]}"
+        
+    def getFunctionDeclaration(self):
+        #fudge funcName(blah, blah, blah):
+        outputStr = "def "
+        print(outputStr)
+        startInd = len(SYNTAX_DICTIONARY["def"])
+        outputStr += self.currentLine[startInd:len(self.currentLine)]
+        print(outputStr)
+        return outputStr
+
+    def getForDeclaration(self):
+        # fur (myhomie i = 0; i < 1; i+=1):
+        #fur,(myhomie, i, =, 0;, i, <, 1;, i+=1):
+        outputStr = "for "
+        outputStr = "i"
+
+
+    def getEverythingFromCurLineAfterIndex(self, index):
+        print()
+        
+        
+
+
 
 
 
