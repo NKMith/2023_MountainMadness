6924239671f6425e3d629c64b85c075d3615d545
Geon Woo Kim <gwk1@sfu.ca>
2023-02-18 11:57:54 -0800

    Added varDeclaration statement functionality

--- interpreter.py
+++ interpreter.py
@@ -12,25 +12,41 @@ Translate
 
 """
 
+INPUT_FILENAME = "syntax.txt"
 
 class Translator:
-    def __init__(self, filename):
-        self.file = open(filename, "r")
-        self.outputFile = 
-
-    # diff funcs for diff stats
+    def __init__(self, inputFileName :str, outputFileName :str):
+        self.file = open(inputFileName, "r")
+        self.outputFile = open(outputFileName, "a")
+        self.currentLine = ""
+        self.currentWordList = ""
 
     def translate(self):
         print("okay")
         # for every line:
             # use first word in line as key to which func to translate
+        
+        self.currentLine = self.file.readline()
+        self.currentWordList = self.currentLine.split()
+        while self.currentLine != "":
+            self.varDeclaration()
+
+    def determineWhichStatement(self):
+        # bunch of different if statements
+        firstWord = self.currentWordList[0]
+        if firstWord == "myHomie":
+            self.varDeclaration()
 
     def varDeclaration(self, line :str):
-        print("Okay")
+        wordList = line.split(" ")
+        outputStr = f"{wordList[0]} = {wordList[2]}"
+        self.outputFile.write(outputStr)
+        
 
 
 
 def main():
+
     print("OKAY")
 
 
