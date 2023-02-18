9bfa82ac5b866f76b2e4bb4fe4051dcda248323a
Geon Woo Kim <gwk1@sfu.ca>
2023-02-18 13:29:02 -0800

    Fixed newline errors in currentLine

--- interpreter.py
+++ interpreter.py
@@ -12,13 +12,14 @@ Translate
 
 """
 
-INPUT_FILENAME = "inputText.txt"
+INPUT_FILENAME = "inputCode.txt"
 OUTPUT_FILENAME = "output.py"
 
 SYNTAX_DICTIONARY = {
     "var" : "myhomie",
     "def" : "fudge",
-    "for" : "fur"
+    "for" : "fur",
+    "print" : "whatDoYouWant"
 }
 
 
@@ -29,6 +30,9 @@ class Translator:
         self.currentLine = ""
         self.currentWordList = ""
 
+    def removeNewlineFromCurrentLine(self):
+        if self.currentLine[len(self.currentLine)-1] == '\n':
+            self.currentLine = self.currentLine[0:len(self.currentLine)-1]
 
     def translate(self):
         print("okay")
@@ -36,13 +40,17 @@ class Translator:
             # use first word in line as key to which func to translate
 
         for line in self.file:
-            print(line)
+            if line == "\n":
+                continue #TODO - Try to get rid of this
+
             self.currentLine = line
+            self.removeNewlineFromCurrentLine()
+            print("INPUT: " + self.currentLine)
             self.currentWordList = self.currentLine.split()
 
             outputStr = self.getTab()
             outputStr += self.determineWhichStatement()
-            print(outputStr)
+            print("WRITING STR: " + outputStr)
 
             self.outputFile.write(outputStr + '\n')
 
@@ -61,19 +69,21 @@ class Translator:
             return self.getVarDeclaration()
         elif firstWord == SYNTAX_DICTIONARY["def"]:
             return self.getFunctionDeclaration()
+
+        if SYNTAX_DICTIONARY["print"] in self.currentLine:
+            return self.currentLine.replace(SYNTAX_DICTIONARY["print"], "print")
         
-        else:
-            return "ERROR - NO SYNTAX MATCHED"
+        return "ERROR - NO SYNTAX MATCHED"
 
     def getVarDeclaration(self):
         # myHomie i = 100
-        print("VAR")
+        print("VAR DECLARATION")
         return f"{self.currentWordList[1]} = {self.currentWordList[3]}"
         
     def getFunctionDeclaration(self):
         #fudge funcName(blah, blah, blah):
+        print("FUNCTION DECLARATION")
         outputStr = "def "
-        print(outputStr)
         startInd = len(SYNTAX_DICTIONARY["def"])
         outputStr += self.currentLine[startInd:len(self.currentLine)]
         print(outputStr)
@@ -88,17 +98,21 @@ class Translator:
 
     def getEverythingFromCurLineAfterIndex(self, index):
         print()
+
+    def getStringBetweenParantheses(self, instr :str):
+        startInd = instr.index("(")
+        lastInd = instr.index(")")
+        return str[startInd:lastInd+1]
         
         
-
-
-
 
 
 def main():
     t = Translator(INPUT_FILENAME, OUTPUT_FILENAME)
     t.translate()
-    print("OKAY")
+    import output
+    
+    print("PROGRAM FINISH")
 
 
 main()
